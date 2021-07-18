646a6dee9cda08f18d4a60d0dc67f6196260ac29
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-07-18 13:38:09 -0300

    React: Estado

--- 03.componentes_tipo_classe/primeiro_projeto_react/meu-app/src/index.js
+++ 03.componentes_tipo_classe/primeiro_projeto_react/meu-app/src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import App2 from './components/App2';
+import App3 from './components/App3';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App2 title="Título">
-      conteudo
-      </App2>
-
-      <App2 title="Título2">
-      conteudo
-      </App2>
+    <App3/>
   </React.StrictMode>,
   document.getElementById('root')
 );
