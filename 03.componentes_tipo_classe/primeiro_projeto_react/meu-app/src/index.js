4e6258d7ab41e4aaba47917b41d3758a20df4a05
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-07-18 15:29:57 -0300

    React: renderizacao de listas

--- 03.componentes_tipo_classe/primeiro_projeto_react/meu-app/src/index.js
+++ 03.componentes_tipo_classe/primeiro_projeto_react/meu-app/src/index.js
@@ -5,11 +5,12 @@ import App2 from './components/App2';
 import App3 from './components/App3';
 import App4 from './components/App4';
 import App5 from './components/App5';
+import Lista2 from './components/Lista2';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App5/>
+    <Lista2/>
   </React.StrictMode>,
   document.getElementById('root')
 );
